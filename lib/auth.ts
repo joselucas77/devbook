af13dc1d8176d29622c2ea626725cf181e0105fb
José Lucas <jose_lucas77@outlook.com>
2026-01-19 20:36:30 -0300

    fix: update authentication flow to use new signIn method and adjust baseURL configuration

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,16 +1,20 @@
 import { betterAuth } from "better-auth";
 import { prisma } from "./prisma";
 import { prismaAdapter } from "better-auth/adapters/prisma";
+import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
-  trustedOrigins: ["https://devbook-eight.vercel.app"],
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
+  emailAndPassword: {
+    enabled: true,
+  },
   socialProviders: {
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     },
   },
+  plugins: [nextCookies()],
 });
