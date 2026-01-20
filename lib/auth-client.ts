f5cb310c6760e0dea14ba55149203e6b83c33582
José Lucas <jose_lucas77@outlook.com>
2026-01-19 21:31:52 -0300

    fix: update login form to use authClient for Google sign-in and remove deprecated signIn function

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -3,10 +3,3 @@ import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
 });
-
-export const signIn = async () => {
-  const data = await authClient.signIn.social({
-    provider: "google",
-    callbackURL: "http://localhost:3000/admin",
-  });
-};
